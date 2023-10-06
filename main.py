617cf7c676d4ec552de6e178ed84ac669abbd3fb
nesladka <147166345+nesladka@users.noreply.github.com>
2023-10-06 23:32:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,7 @@ plt.figure(figsize=(8, 6))
 plt.xlabel('x1')
 plt.ylabel('x2')
 
-plt.plot(x1, (141 - 4 * x1) / 5, label='4*X1 + 5*X2 = 141', color='blue', linestyle='dotted') 
+plt.plot(x1, (141 - 4 * x1) / 5, label='4*X1 + 5*X2 = 141', color='blue', linestyle='dotted')
+
+plt.axvline(x=19, color='purple', linestyle='dotted', label='x1=19')  
+plt.axhline(y=17, color='orange', linestyle='dotted', label='x2=17') 
