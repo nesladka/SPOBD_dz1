bb775777ce6c9743e2c15270d481920bd9d7a451
nesladka <147166345+nesladka@users.noreply.github.com>
2023-10-06 23:32:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,5 @@ X1, X2 = np.meshgrid(x1, x2)
 plt.figure(figsize=(8, 6))
 plt.xlabel('x1')
 plt.ylabel('x2')
+
+plt.plot(x1, (141 - 4 * x1) / 5, label='4*X1 + 5*X2 = 141', color='blue', linestyle='dotted') 
