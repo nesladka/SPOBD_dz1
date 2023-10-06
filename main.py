fbef844421838840368618bd5f8a267323e1e4a6
nesladka <147166345+nesladka@users.noreply.github.com>
2023-10-06 23:33:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,6 @@ plt.axvline(x=19, color='purple', linestyle='dotted', label='x1=19')
 plt.axhline(y=17, color='orange', linestyle='dotted', label='x2=17') 
 
 plt.arrow(0, 0, 3, 5, head_width=0.5, head_length=0.7, fc='red', ec='red', label='вектор z (3, 5)') 
+
+perpendicular_x = np.array([5, -5])
+perpendicular_y = np.array([-3, 3])
